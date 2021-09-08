da32cfd5080d3642ebaa04e685aba88a4f7aeaf1
bssherwood <bssherwood@users.noreply.github.com>
2021-09-08 16:33:45 -0500

    Update test.R

--- ignore/test.R
+++ ignore/test.R
@@ -3,24 +3,71 @@ library(devtools)
 devtools::unload("rqPen")
 devtools::unload("hrqglas")
 install_github("shaobo-li/hrqglas")
+3
 install_github("bssherwood/rqpen")
+3
 library(rqPen)
 library(hrqglas)
 library(quantreg)
 data(barro)
 y <- barro$y.net
-x <- barro[,-1]
+x <- as.matrix(barro[,-1])
 
 groups <- c(rep(1,4),rep(2,3),rep(3,3),rep(4,3))
 
 
 #r1 <- rq.pen(x,y)
 lamMax <- rqPen:::getLamMaxGroup(x, y, groups, .5, rep(1,4), 
-                         penalty = "gLASSO", TRUE)
+                         penalty = "gLASSO", scalex=TRUE)
 eps <- ifelse(nrow(x) < ncol(x), 0.01, 1e-04)
 nlambda <- 100
 lambda <- exp(seq(log(lamMax), log(eps * lamMax), length.out = nlambda))
-hrq_glasso(x,y,groups,tau=.5,lambda=lambda)
+
+h0 <- hrq_glasso(x,y,groups,tau=.5)
+h1 <- hrq_glasso(x,y,groups,tau=.5,lambda=lambda)
+r1 <- rq.group.pen(x,y,groups=groups)
+
+
+getLamMaxGroup <- function(x,y,group.index,tau=.5,group.pen.factor,gamma=.2,gamma.max=4,gamma.q=.1,penalty="gLASSO",scalex=TRUE){
+  # code improvement: Hacky approach to the group.pen.factor issue. 
+  returnVal <- 0
+  n <- length(y)
+  if(scalex){
+    x <- scale(x)
+  }
+  validSpots <- which(group.pen.factor!=0)
+  for(tau_val in tau){
+    r <- y - quantile(y,tau_val)
+    gamma0<- min(gamma.max, max(gamma, quantile(abs(r), probs = gamma.q)))
+    
+    grad_k<- -neg.gradient(r, rep(1,n), tau_val, gamma=gamma0, x, apprx="huber")
+    grad_k.norm<- tapply(grad_k, group.index, l2norm)
+    
+    lambda.max<- max(c(returnVal,grad_k.norm[validSpots]/group.pen.factor[validSpots]))
+  }
+  lambda.max
+}
+
+n <- length(y)
+tau <- .5
+gamma.max <- 4
+gamma0 <- .2
+weights <- rep(1,n)
+group.index <- groups
+
+b.int<- quantile(y, probs = tau)
+r<- y-b.int
+gamma.max<- 4; 
+gamma<- min(gamma.max, max(gamma0, quantile(abs(r), probs = 0.1)))
+
+r0 <- r
+apprx <- "huber"
+grad_k<- -hrqglas:::neg.gradient(r0, weights, tau, gamma=gamma, x, n, apprx)
+grad_k.norm<- tapply(grad_k, group.index, hrqglas:::l2norm)
+
+lambda.max<- max(grad_k.norm/w.lambda)
+
+
 #figure out the difference in the lambda maximum values. 
 fit <- hrq_glasso(x,y,groups,tau=.5)
 
